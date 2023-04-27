290f965363891e1508ac897a96c068bc791f50b0
mrzizik <mrfreezzzer@mail.ru>
2023-04-27 16:21:27 +0300

    version increment

--- oauth/build.gradle.kts
+++ oauth/build.gradle.kts
@@ -148,7 +148,7 @@ kapt {
     correctErrorTypes = true
 }
 
-val currentVersion by extra("0.0.37")
+val currentVersion by extra("0.0.38")
 publishing {
     publications {
         register<MavenPublication>("release") {
