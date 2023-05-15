43e8a79476decd0669161c96fc929260ebeb134e
arvifox <arvifox@hotmail.com>
2023-05-15 16:58:44 +0300

    update sdk

--- oauth/build.gradle.kts
+++ oauth/build.gradle.kts
@@ -151,7 +151,7 @@ kapt {
     correctErrorTypes = true
 }
 
-val currentVersion by extra("0.0.38")
+val currentVersion by extra("0.0.40")
 publishing {
     publications {
         register<MavenPublication>("release") {
