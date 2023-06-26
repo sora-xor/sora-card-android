b5e61e33d40a58af68bc8cde5e8548e656241114
arvifox <arvifox@hotmail.com>
2023-06-26 23:45:41 +0300

    sca-26

--- oauth/build.gradle.kts
+++ oauth/build.gradle.kts
@@ -151,7 +151,7 @@ kapt {
     correctErrorTypes = true
 }
 
-val currentVersion by extra("0.0.44")
+val currentVersion by extra("0.0.45")
 publishing {
     publications {
         register<MavenPublication>("release") {
