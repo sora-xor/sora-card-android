cc141089ffb4606bfc8f9dde94792c9bab0b52c4
Ali Abdulmadzhidov <mrfreezzzer@mail.ru>
2023-08-02 13:57:44 +0300

    increment ui-core version

--- oauth/build.gradle.kts
+++ oauth/build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 }
 
 val composeCompilerVersion by extra("1.4.6")
-val uiCoreVersion by extra("0.0.74")
+val uiCoreVersion by extra("0.0.75")
 val hiltVersion by extra("2.45")
 val pwOauthSdkVersion by extra("1.3.1")
 val pwKycSdkVersion by extra("4.5.0")
