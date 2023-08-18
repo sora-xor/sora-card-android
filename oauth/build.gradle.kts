2e213e7a81c824abbdbbfda4f43ae4fefb449511
AmadeyKuspakov <SteveDrumble1@gmail.com>
2023-08-18 15:44:12 +0400

    Increased lib version

--- oauth/build.gradle.kts
+++ oauth/build.gradle.kts
@@ -141,7 +141,7 @@ kapt {
     correctErrorTypes = true
 }
 
-val currentVersion by extra("0.1.30")
+val currentVersion by extra("0.1.31")
 publishing {
     publications {
         register<MavenPublication>("release") {
