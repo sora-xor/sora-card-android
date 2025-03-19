316ade023bb162dedfa1019acd57e6bbc9a9f1b3
Ekaterina <lokatin21@gmail.com>
2025-03-19 13:13:37 +0300

    migrate to k2, update versions, v1.2.0-RC10-K2

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     id("org.jetbrains.dokka") version "1.9.20" apply false
     id("org.sonarqube") version "4.4.1.3373"
     alias(libs.plugins.jetbrainsKotlinJvm) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
 
 tasks.register("clean", Delete::class) {
